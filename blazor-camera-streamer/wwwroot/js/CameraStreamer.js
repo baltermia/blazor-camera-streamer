a2cc74284cc84dba5da4d5173b4d6356064a48ab
speyck <54800864+speyck@users.noreply.github.com>
2021-08-31 12:52:59 +0200

    Add compiled ts script as js in wwwroot

---
+++ blazor-camera-streamer/wwwroot/js/CameraStreamer.js
@@ -0,0 +1,12 @@
+var blazor_camera_streamer;
+(function (blazor_camera_streamer) {
+    var Scripts;
+    (function (Scripts) {
+        var CameraStreamer = /** @class */ (function () {
+            function CameraStreamer() {
+            }
+            return CameraStreamer;
+        }());
+    })(Scripts = blazor_camera_streamer.Scripts || (blazor_camera_streamer.Scripts = {}));
+})(blazor_camera_streamer || (blazor_camera_streamer = {}));
+//# sourceMappingURL=CameraStreamer.js.map
