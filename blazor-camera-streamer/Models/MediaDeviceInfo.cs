3d645d33e7f644b7cca9c099bcf63b0bb465912c
speyck <54800864+speyck@users.noreply.github.com>
2021-09-09 15:17:04 +0200

    Create MediaDeviceInfo.cs

---
+++ blazor-camera-streamer/Models/MediaDeviceInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorCameraStreamer.Models
+{
+    public class MediaDeviceInfo
+    {
+        public string DeviceId { get; set; }
+        public string Label { get; set; }
+    }
+}
