e7615a348876f0478c8a99dd185be0a1224ea04d
speyck <54800864+speyck@users.noreply.github.com>
2021-09-09 15:20:45 +0200

    Add getMediaDeviceInfos method in script

--- blazor-camera-streamer/Scripts/CameraStreamer.ts
+++ blazor-camera-streamer/Scripts/CameraStreamer.ts
@@ -1,9 +1,53 @@
 namespace BlazorCameraStreamer.Scripts {
+    /**
+     * This class is designed to work with Microsoft.JSInterop in C# Blazor and helps streaming a camera on a canvas
+     */
     export class CameraStreamerInterop {
-        private constructor() { }
+        private _video: HTMLVideoElement;
+        private _api: any;
 
-        public static CreateInstance(): CameraStreamerInterop {
+        /**
+         * Returns a new instance of the CameraStreamerInterop class
+         */
+        public static createInstance(): CameraStreamerInterop {
             return new CameraStreamerInterop();
+        }
+
+        /**
+         * Checks if the site has access to the camera
+         * @param ask wether the method should ask the user if he wants to grant access if the access is currently denied
+         * @returns whether the site can access the camera
+         */
+        public static getCameraAccess(ask: boolean = true) : boolean {
+            return true;
+        }
+
+        public static getCameraDeviceList(): Promise<MediaDeviceInfo[]> {
+            return navigator.mediaDevices.enumerateDevices().then(l => l.filter(d => d.kind === "videoinput"));
+        }
+
+        /**
+         * Initializes the camera streamer - wihtout initializing the stream wont work
+         * @param video ElementReference of the video
+         * @param api TODO: this
+         * @param camera Device-string (id) of the camera that should be used for the stream
+         */
+        public initialize(video: HTMLVideoElement, api: any, camera: string, width: number = 480, height: number = 270): void{
+            this._video = video;
+            this._api = api;
+
+            this._video.height
+
+            // Subscribe to onseeked event (you can't use '+=' like in c#)
+            this._video.onseeked = this.video_onSeeked;
+        }
+
+        private video_onSeeked(e: Event) {
+
+        }
+
+        public dispose(): void {
+            
         }
     }
 }
--- blazor-camera-streamer/wwwroot/js/CameraStreamer.js
+++ blazor-camera-streamer/wwwroot/js/CameraStreamer.js
@@ -2,11 +2,48 @@ var BlazorCameraStreamer;
 (function (BlazorCameraStreamer) {
     var Scripts;
     (function (Scripts) {
-        var CameraStreamerInterop = (function () {
+        /**
+         * This class is designed to work with Microsoft.JSInterop in C# Blazor and helps streaming a camera on a canvas
+         */
+        var CameraStreamerInterop = /** @class */ (function () {
             function CameraStreamerInterop() {
             }
-            CameraStreamerInterop.CreateInstance = function () {
+            /**
+             * Returns a new instance of the CameraStreamerInterop class
+             */
+            CameraStreamerInterop.createInstance = function () {
                 return new CameraStreamerInterop();
+            };
+            /**
+             * Checks if the site has access to the camera
+             * @param ask wether the method should ask the user if he wants to grant access if the access is currently denied
+             * @returns whether the site can access the camera
+             */
+            CameraStreamerInterop.getCameraAccess = function (ask) {
+                if (ask === void 0) { ask = true; }
+                return true;
+            };
+            CameraStreamerInterop.getCameraDeviceList = function () {
+                return navigator.mediaDevices.enumerateDevices().then(function (l) { return l.filter(function (d) { return d.kind === "videoinput"; }); });
+            };
+            /**
+             * Initializes the camera streamer - wihtout initializing the stream wont work
+             * @param video ElementReference of the video
+             * @param api TODO: this
+             * @param camera Device-string (id) of the camera that should be used for the stream
+             */
+            CameraStreamerInterop.prototype.initialize = function (video, api, camera, width, height) {
+                if (width === void 0) { width = 480; }
+                if (height === void 0) { height = 270; }
+                this._video = video;
+                this._api = api;
+                this._video.height;
+                // Subscribe to onseeked event (you can't use '+=' like in c#)
+                this._video.onseeked = this.video_onSeeked;
+            };
+            CameraStreamerInterop.prototype.video_onSeeked = function (e) {
+            };
+            CameraStreamerInterop.prototype.dispose = function () {
             };
             return CameraStreamerInterop;
         }());
