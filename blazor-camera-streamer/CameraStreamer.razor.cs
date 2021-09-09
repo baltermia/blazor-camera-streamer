a34ce575ff9a13175e089702e356a5cb5e8f54ee
speyck <54800864+speyck@users.noreply.github.com>
2021-09-09 15:19:43 +0200

    Add Parameters and GetDevices method in CameraStreamer component

--- blazor-camera-streamer/CameraStreamer.razor.cs
+++ blazor-camera-streamer/CameraStreamer.razor.cs
@@ -1,5 +1,7 @@
+using BlazorCameraStreamer.Models;
 using Microsoft.AspNetCore.Components;
 using Microsoft.JSInterop;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace BlazorCameraStreamer
@@ -9,15 +11,39 @@ namespace BlazorCameraStreamer
         [Inject]
         private IJSRuntime JSRuntime { get; set; }
 
+        [Parameter]
+        public string Style { get; set; } = string.Empty;
+
+        [Parameter]
+        public string Id { get; set; } = string.Empty;
+
+        [Parameter]
+        public string Class { get; set; } = string.Empty;
+
+        [Parameter]
+        public EventCallback<string> OnFrame { get; set; }
+
         protected ElementReference VideoRef { get; set; }
 
         protected CameraStreamerAPI streamerApi;
 
-        protected override void OnInitialized()
+        protected override async Task OnAfterRenderAsync(bool firstRender)
         {
-            streamerApi = new CameraStreamerAPI(JSRuntime);
+            if (firstRender)
+            {
+                streamerApi = new CameraStreamerAPI(JSRuntime);
 
-            base.OnInitialized();
+                await ReloadAsync();
+            }
+
+            await base.OnAfterRenderAsync(firstRender);
         }
+
+        public async Task ReloadAsync()
+        {
+            await streamerApi.InitializeAsync();
+        }
+
+        public async Task<IEnumerable<MediaDeviceInfo>> GetMediaDeviceInfoAsync() => await streamerApi.GetCameraDevicesAsync();
     }
 }
