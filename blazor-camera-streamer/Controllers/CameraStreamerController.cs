cd001d31bf1a79ad67a1c9cc1516c32685b5e674
speyck <54800864+speyck@users.noreply.github.com>
2021-09-15 16:08:25 +0200

    Implement typescript callback-invoke OnFrame and added docs

--- blazor-camera-streamer/Controllers/CameraStreamerController.cs
+++ blazor-camera-streamer/Controllers/CameraStreamerController.cs
@@ -11,33 +11,68 @@ namespace BlazorCameraStreamer
     /// </summary>
     public class CameraStreamerController : ICameraStreamerModel
     {
-        public const string JS_STATIC = "BlazorCameraStreamer.Scripts.CameraStreamerInterop";
-        public readonly IJSRuntime JSRuntime;
+        /// <summary>
+        /// Path that can be used to access the static methods in the CameraStreamer typescript file
+        /// </summary>
+        private const string StaticInteropPath = "BlazorCameraStreamer.Scripts.CameraStreamerInterop";
+
+        /// <summary>
+        /// Runtime used to use javascript interopability
+        /// </summary>
+        private readonly IJSRuntime JSRuntime;
         
+        /// <summary>
+        /// Typescript CameraStreamer object
+        /// </summary>
         private IJSObjectReference JSObject;
+        
+        /// <summary>
+        /// States if the object has been initialized already
+        /// </summary>
         public bool IsInitialized { get; private set; } = false;
 
-        internal CameraStreamerController(IJSRuntime runtime)
+        /// <summary>
+        /// Is called on each frame of the camera stream with a base64 string of the image
+        /// </summary>
+        private EventCallback<string> OnFrameCallback;
+
+        /// <summary>
+        /// Creates a new instance of the CameraStreamerController class
+        /// </summary>
+        /// <param name="runtime">Runtime used for javascript interopability</param>
+        public CameraStreamerController(IJSRuntime runtime)
         {
             JSRuntime = runtime;
         }
 
-        public async Task InitializeAsync(ElementReference videoReference, int width = 480, int height = 270)
+        /// <summary>
+        /// Initializes the typescript object with the provided parameters
+        /// </summary>
+        /// <param name="videoReference"></param>
+        /// <param name="width"></param>
+        /// <param name="height"></param>
+        /// <param name="onFrameCallback"></param>
+        /// <returns></returns>
+        public async Task InitializeAsync(ElementReference videoReference, int width = 640, int height = 360, EventCallback<string> onFrameCallback = default)
         {
             if (IsInitialized)
             {
                 await DisposeAsync();
             }
 
-            JSObject = await JSRuntime.InvokeAsync<IJSObjectReference>(JS_STATIC + ".createInstance");
+            OnFrameCallback = onFrameCallback;
 
-            await JSObject.InvokeVoidAsync("init", videoReference, DotNetObjectReference.Create(this), width, height);
+            JSObject = await JSRuntime.InvokeAsync<IJSObjectReference>(StaticInteropPath + ".createInstance");
+
+
+            await JSObject.InvokeVoidAsync("init", videoReference, OnFrameCallback.HasDelegate, DotNetObjectReference.Create(this), nameof(OnFrame), width, height);
 
             IsInitialized = true;
         }
 
         public async Task StartAsync(string camera = null)
         {
+            // Use the first found camera if no camrea is given
             await JSObject.InvokeVoidAsync("start", camera ?? (await GetCameraDevicesAsync()).FirstOrDefault()?.DeviceId);
         }
 
@@ -51,9 +86,9 @@ namespace BlazorCameraStreamer
             await JSObject.InvokeVoidAsync("changeCamera", newId);
         }
 
-        public async Task<bool> GetCameraAccessAsync(bool ask = true)
+        public async Task<bool> GetCameraAccessAsync()
         {
-            return await JSObject.InvokeAsync<bool>("getCameraAccess", ask);
+            return await JSRuntime.InvokeAsync<bool>(StaticInteropPath + ".getCameraAccess");
         }
 
         public async Task<MediaDeviceInfoModel[]> GetCameraDevicesAsync()
@@ -69,9 +104,24 @@ namespace BlazorCameraStreamer
             }
         }
 
+        /// <summary>
+        /// Gets all camera devices (no audio) as MediaDeviceInfo and returns them as an array
+        /// </summary>
+        /// <returns>An array of all cameras as MediaDeviceInfo</returns>
         public static async Task<MediaDeviceInfoModel[]> GetCameraDevicesAsync(IJSRuntime runtime)
         {
-            return await runtime.InvokeAsync<MediaDeviceInfoModel[]>(JS_STATIC + ".getCameraDeviceList");
+            return await runtime.InvokeAsync<MediaDeviceInfoModel[]>(StaticInteropPath + ".getCameraDeviceList");
+        }
+
+        /// <summary>
+        /// Invokable method from javascript/typescript that calls the given callback method
+        /// </summary>
+        /// <param name="data">Base64 string of the image</param>
+        [JSInvokable]
+        public async Task OnFrame(string data)
+        {
+            if (OnFrameCallback.HasDelegate)
+                await OnFrameCallback.InvokeAsync(data);
         }
     }
 }
