1918b4d789d737926338aff9d847f73aab402e5c
baltermia <54800864+baltermia@users.noreply.github.com>
2023-05-03 08:41:59 +0200

    Fix BlazorCameraStreamer.Demo.WASM-Namespaces

--- BlazorCameraStreamer.Demo.WASM/Program.cs
+++ BlazorCameraStreamer.Demo.WASM/Program.cs
@@ -8,7 +8,7 @@ using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace blazor_camera_streamer.Demo
+namespace BlazorCameraStreamer.Demo.WASM
 {
     public class Program
     {
